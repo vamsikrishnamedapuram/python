55e49f846b18a68d9c5096873bb6c4c95bee7097
vamsikrishnamedapuram <63710672+vamsikrishnamedapuram@users.noreply.github.com>
2023-11-13 12:57:17 +0000

    pratice file

---
+++ day-05/practice.py
@@ -0,0 +1,24 @@
+import sys
+def sum(num1, num2):
+    sum = num1 + num2
+    return sum
+
+def sub(num1, num2):
+    sub = num1 - num2
+    return sub
+
+def mul(num1, num2):
+    mul = num1 * num2
+    return mul
+
+num1 = sys.argv[1]
+operation = sys.argv[2]
+num2 = sys.argv[3]
+
+if operation == "sum"
+    result = sum(num1, num2)
+    print(result)
+
+if operation == "sub"
+    result = sum(num1, num2)
+    print(result)
